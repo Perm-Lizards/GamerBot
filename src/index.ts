6b098956b4aacd3efd0668cf43ccebb294927653
Koichi-hub <koichi.hub@gmail.com>
2022-10-02 20:08:04 +0500

    Рефакторинг

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Bot } from "grammy";
-import { BOT_TOKEN } from "./config"
+import { Bot } from 'grammy';
+import { BOT_TOKEN } from './config';
 
 // Create a bot object
 const bot = new Bot(BOT_TOKEN as string); // <-- place your bot token in this string
 
 // Register listeners to handle messages
-bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
+bot.on('message:text', (ctx) => ctx.reply('Echo: ' + ctx.message.text));
 
 // Start the bot (using long polling)
 bot.start();
