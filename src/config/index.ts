9757696411014328cc55eb75f2e3e7793303780c
Koichi-hub <koichi.hub@gmail.com>
2022-11-09 00:30:27 +0500

    Веб-приложение и конфига

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,8 @@
-import * as dotenv from 'dotenv';
-import path from 'path';
+import * as dotenv from "dotenv";
+import path from "path";
 
 dotenv.config({
-  path: path.join(__dirname, '../.env'),
+  path: path.join(__dirname, "../../.env"),
 });
 
-const { BOT_TOKEN } = process.env;
-
-if (!(BOT_TOKEN ?? false)) {
-  throw new Error('No telegram bot token provided');
-}
-
-export { BOT_TOKEN };
+export const { BOT_TOKEN, WEB_APP_HOST } = process.env;
