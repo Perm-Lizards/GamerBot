13911a7dbad28f3b52f605dd8d320bdff20592d8
Koichi-hub <gogotimur2020@gmail.com>
2023-03-16 18:45:34 +0500

    Добавил приветствие

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,10 @@ import { BOT_TOKEN } from './config';
 
 const bot = new Bot(BOT_TOKEN as string);
 
+bot.command('start', (ctx) => {
+  ctx.reply(
+    'Приветствую! Я Телеграмм бот проекта GamerBot. Нажми на кнопку "GamerBot" чтобы открыть приложение'
+  );
+});
+
 bot.start();
